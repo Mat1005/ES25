1283971b032b0d43a2fa08896dd544c20a5fe407
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-02-05 18:03:18 +0100

    Update Es25Pag190.py

--- Es25Pag190.py
+++ Es25Pag190.py
@@ -9,8 +9,103 @@ riducendo a uno i voti assegnati.
 '''
 n_studenti = 30
 studenti_voti = {}
-for n in range(1, n_studenti +1):
-    studente = input("inserire il nome dello studente")
-    voto = float(input("Inserire il voto"))
-    studenti_voti = {studente:voto}
+studente1 = input("inserire il nome dello studente")
+voto1 = float(input("Inserire il voto"))
+studente2 = input("inserire il nome dello studente")
+voto2 = float(input("Inserire il voto"))
+studente3 = input("inserire il nome dello studente")
+voto3 = float(input("Inserire il voto"))
+studente4 = input("inserire il nome dello studente")
+voto4 = float(input("Inserire il voto"))
+studente5 = input("inserire il nome dello studente")
+voto5 = float(input("Inserire il voto"))
+studente6 = input("inserire il nome dello studente")
+voto6 = float(input("Inserire il voto"))
+studente7 = input("inserire il nome dello studente")
+voto7 = float(input("Inserire il voto"))
+studente8 = input("inserire il nome dello studente")
+voto8 = float(input("Inserire il voto"))
+studente9 = input("inserire il nome dello studente")
+voto9 = float(input("Inserire il voto"))
+studente10 = input("inserire il nome dello studente")
+voto10 = float(input("Inserire il voto"))
+studente11 = input("inserire il nome dello studente")
+voto11 = float(input("Inserire il voto"))
+studente12 = input("inserire il nome dello studente")
+voto12 = float(input("Inserire il voto"))
+studente13 = input("inserire il nome dello studente")
+voto13 = float(input("Inserire il voto"))
+studente14 = input("inserire il nome dello studente")
+voto14 = float(input("Inserire il voto"))
+studente15 = input("inserire il nome dello studente")
+voto15 = float(input("Inserire il voto"))
+studente16 = input("inserire il nome dello studente")
+voto16 = float(input("Inserire il voto"))
+studente17 = input("inserire il nome dello studente")
+voto17 = float(input("Inserire il voto"))
+studente18 = input("inserire il nome dello studente")
+voto18 = float(input("Inserire il voto"))
+studente19 = input("inserire il nome dello studente")
+voto19 = float(input("Inserire il voto"))
+studente20 = input("inserire il nome dello studente")
+voto20 = float(input("Inserire il voto"))
+studente21 = input("inserire il nome dello studente")
+voto21 = float(input("Inserire il voto"))
+studente22 = input("inserire il nome dello studente")
+voto23 = float(input("Inserire il voto"))
+studente24 = input("inserire il nome dello studente")
+voto24 = float(input("Inserire il voto"))
+studente25 = input("inserire il nome dello studente")
+voto25 = float(input("Inserire il voto"))
+studente26 = input("inserire il nome dello studente")
+voto26 = float(input("Inserire il voto"))
+studente27 = input("inserire il nome dello studente")
+voto27 = float(input("Inserire il voto"))
+studente28 = input("inserire il nome dello studente")
+voto28 = float(input("Inserire il voto"))
+studente29 = input("inserire il nome dello studente")
+voto29 = float(input("Inserire il voto"))
+studente30 = input("inserire il nome dello studente")
+voto30 = float(input("Inserire il voto"))
+Studenti = [studente1,studente2,studente3,studente4,studente5,
+            studente6,studente7,studente8,studente9,studente10,
+            studente11,studente12,studente13,studente14,studente15,
+            studente16,studente17,studente18,
+            studente19,studente20,studente21,studente22,
+            studente23,studente24,studente25,studente26,
+            studente27,studente28,studente29,studente30]
+Voti = [voto1,voto2,voto3,voto4,voto5,voto6,voto7,voto8,voto9,voto10,
+        voto11,voto12,voto13,voto14,voto15,voto16,voto17,voto18,voto19,
+        voto20,voto21,voto22,voto23,voto24,voto25,voto26,voto27,voto28,voto29,voto30]
+
+studenti_voti = {studente1:voto1,
+                 studente2:voto2,
+                 studente3:voto3,
+                 studente4:voto4,
+                 studente5:voto5,
+                 studente6:voto6,
+                 studente7:voto7,
+                 studente8:voto8,
+                 studente9:voto9,
+                 studente10:voto10,
+                 studente11:voto11,
+                 studente12:voto12,
+                 studente13:voto13,
+                 studente14:voto14,
+                 studente15:voto15,
+                 studente16:voto16,
+                 studente17:voto17,
+                 studente18:voto18,
+                 studente19:voto19,
+                 studente20:voto20,
+                 studente21:voto21,
+                 studente22:voto22,
+                 studente23:voto23,
+                 studente24:voto24,
+                 studente25:voto25,
+                 studente26:voto26,
+                 studente27:voto27,
+                 studente28:voto28,
+                 studente29:voto29,
+                 studente30:voto30}
 print(studenti_voti)
