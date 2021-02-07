1787f03127778bdfb0505a90b55709501d74d296
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-02-07 10:08:49 +0100

    Update Es25Pag190.py

--- Es25Pag190.py
+++ Es25Pag190.py
@@ -7,105 +7,81 @@ Elenca i risultati in ordine crescente di voto e successivamente visualizza
 quali voti diversi tra loro sono stati assegnati,
 riducendo a uno i voti assegnati.
 '''
-n_studenti = 30
-studenti_voti = {}
-studente1 = input("inserire il nome dello studente")
-voto1 = float(input("Inserire il voto"))
-studente2 = input("inserire il nome dello studente")
-voto2 = float(input("Inserire il voto"))
-studente3 = input("inserire il nome dello studente")
-voto3 = float(input("Inserire il voto"))
-studente4 = input("inserire il nome dello studente")
-voto4 = float(input("Inserire il voto"))
-studente5 = input("inserire il nome dello studente")
-voto5 = float(input("Inserire il voto"))
-studente6 = input("inserire il nome dello studente")
-voto6 = float(input("Inserire il voto"))
-studente7 = input("inserire il nome dello studente")
-voto7 = float(input("Inserire il voto"))
-studente8 = input("inserire il nome dello studente")
-voto8 = float(input("Inserire il voto"))
-studente9 = input("inserire il nome dello studente")
-voto9 = float(input("Inserire il voto"))
-studente10 = input("inserire il nome dello studente")
-voto10 = float(input("Inserire il voto"))
-studente11 = input("inserire il nome dello studente")
-voto11 = float(input("Inserire il voto"))
-studente12 = input("inserire il nome dello studente")
-voto12 = float(input("Inserire il voto"))
-studente13 = input("inserire il nome dello studente")
-voto13 = float(input("Inserire il voto"))
-studente14 = input("inserire il nome dello studente")
-voto14 = float(input("Inserire il voto"))
-studente15 = input("inserire il nome dello studente")
-voto15 = float(input("Inserire il voto"))
-studente16 = input("inserire il nome dello studente")
-voto16 = float(input("Inserire il voto"))
-studente17 = input("inserire il nome dello studente")
-voto17 = float(input("Inserire il voto"))
-studente18 = input("inserire il nome dello studente")
-voto18 = float(input("Inserire il voto"))
-studente19 = input("inserire il nome dello studente")
-voto19 = float(input("Inserire il voto"))
-studente20 = input("inserire il nome dello studente")
-voto20 = float(input("Inserire il voto"))
-studente21 = input("inserire il nome dello studente")
-voto21 = float(input("Inserire il voto"))
-studente22 = input("inserire il nome dello studente")
-voto23 = float(input("Inserire il voto"))
-studente24 = input("inserire il nome dello studente")
-voto24 = float(input("Inserire il voto"))
-studente25 = input("inserire il nome dello studente")
-voto25 = float(input("Inserire il voto"))
-studente26 = input("inserire il nome dello studente")
-voto26 = float(input("Inserire il voto"))
-studente27 = input("inserire il nome dello studente")
-voto27 = float(input("Inserire il voto"))
-studente28 = input("inserire il nome dello studente")
-voto28 = float(input("Inserire il voto"))
-studente29 = input("inserire il nome dello studente")
-voto29 = float(input("Inserire il voto"))
-studente30 = input("inserire il nome dello studente")
-voto30 = float(input("Inserire il voto"))
-Studenti = [studente1,studente2,studente3,studente4,studente5,
-            studente6,studente7,studente8,studente9,studente10,
-            studente11,studente12,studente13,studente14,studente15,
-            studente16,studente17,studente18,
-            studente19,studente20,studente21,studente22,
-            studente23,studente24,studente25,studente26,
-            studente27,studente28,studente29,studente30]
-Voti = [voto1,voto2,voto3,voto4,voto5,voto6,voto7,voto8,voto9,voto10,
-        voto11,voto12,voto13,voto14,voto15,voto16,voto17,voto18,voto19,
-        voto20,voto21,voto22,voto23,voto24,voto25,voto26,voto27,voto28,voto29,voto30]
+import random as r
+    
+def dizionario_inizializza():
+       
+    while len(studenti) < tot:
+        studenti[r.randint(1,30)] = r.randint(1, 10)
+    
+        
 
-studenti_voti = {studente1:voto1,
-                 studente2:voto2,
-                 studente3:voto3,
-                 studente4:voto4,
-                 studente5:voto5,
-                 studente6:voto6,
-                 studente7:voto7,
-                 studente8:voto8,
-                 studente9:voto9,
-                 studente10:voto10,
-                 studente11:voto11,
-                 studente12:voto12,
-                 studente13:voto13,
-                 studente14:voto14,
-                 studente15:voto15,
-                 studente16:voto16,
-                 studente17:voto17,
-                 studente18:voto18,
-                 studente19:voto19,
-                 studente20:voto20,
-                 studente21:voto21,
-                 studente22:voto22,
-                 studente23:voto23,
-                 studente24:voto24,
-                 studente25:voto25,
-                 studente26:voto26,
-                 studente27:voto27,
-                 studente28:voto28,
-                 studente29:voto29,
-                 studente30:voto30}
-print(studenti_voti)
+def studenti_ordinati():
+    print("Elenco studenti ordinati per voto in modo crescente")
+    for chiave in sorted(studenti, key = studenti.get):
+        print("studente", chiave, "=", studenti[chiave])
+
+def voti_distinti():
+    print("Elenco voti distinti")
+    for voto in set(studenti.values()):
+        print("voto =", voto)
+
+def main():
+    global tot
+    global studenti
+    global classe
+    
+    tot = 30
+    studenti = {}
+    classe = {"Mario Rossi":1,
+              "Enrico Bianchi":2,
+              "Matteo Zaccarelli":3,
+              "Antonio Bruno":4,
+              "Andrea De Luca":5,
+              "Lucia Mondella":6,
+              "Renzo Tramaglino":7,
+              "Marco Fontana":8,
+              "Giorgia Gallo":9,
+              "Aurora Conti":10,
+              "Andra Barbera":11,
+              "Marta Angela":12,
+              "Bartolomeo Ferrero":13,
+              "Andrea Bondavalli":14,
+              "Giovanni Storti":15,
+              "Aldo Baglio":16,
+              "Dante Alighieri":17,
+              "Giacomo Poretti":18,
+              "Alberto Ferrari":19,
+              "Alessia D'Angelo":20,
+              "Arianna Russo":21,
+              "Marco Romano":22,
+              "Martina Esposito":23,
+              "Mariangela Ricci":24,
+              "Giorgio Greco":25,
+              "Lorenzo Rinaldi":26,
+              "Luca Gentile":27,
+              "Irene Leone":28,
+              "Aleessio Longo":29,
+              "Mirco Martinelli":30}
+    dizionario_inizializza()
+    
+    while True:
+        print("\nOperazioni possibili")
+        print("[1] Mostra elenco studenti con voto \n[2] Mostra elenco voti distinti")
+        scelta = input("cosa vuoi fare ")
+        if not scelta.isdigit():
+            print("\nErrore nella digitazione, riprova ...\n")
+        else:
+            scelta_int = int(scelta)
+            if scelta_int == 1:
+                print("Ad ogni voto è associato un numero,tale numero corrisponde ad uno studente,")
+                print("per capire di chi è un voto, consultare il seguente dizionario. ")
+                print(classe)
+                studenti_ordinati()
+                
+            elif scelta_int == 2:
+                voti_distinti()
+                break
+            else:
+                print("\nHai digitato male, riprova ...\n")
+main()
